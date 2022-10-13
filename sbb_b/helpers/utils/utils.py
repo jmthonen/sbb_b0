a885daf2a8d72315ed397888d99eb2eea50d0839
jmthonen <113664229+jmthonen@users.noreply.github.com>
2022-10-13 10:35:17 +0300

    Update utils.py

--- sbb_b/helpers/utils/utils.py
+++ sbb_b/helpers/utils/utils.py
@@ -7,6 +7,19 @@ from ...core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
+
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
+
+
 # executing of terminal commands
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     args = shlex.split(cmd)
